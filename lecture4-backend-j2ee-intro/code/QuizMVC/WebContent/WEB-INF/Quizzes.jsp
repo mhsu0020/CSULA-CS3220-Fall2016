bf10288236239d2b1cec6568304468ba42aacd56
Michael Hsu <pig0826usanj@gmail.com>
2016-09-15 07:56:26 -0700

    added Quiz MVC Example

---
+++ lecture4-backend-j2ee-intro/code/QuizMVC/WebContent/WEB-INF/Quizzes.jsp
@@ -0,0 +1,28 @@
+<!-- This is a comment directive. below are JSTL directives. Note that in the final html returned to client it doe not show up.-->
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+  <!-- The prefix shows up in the core tags you use -->
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Quizzes</title>
+	<!-- Files in WEB-INF are not publicly accessible. the css file is located in the css folder under the WebContent directory. -->
+	 <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<h1><c:out value="${pageHeader}" /></h1>
+	<!-- You can still add classes to the elements, just like regular html -->
+	<table class="quiz-table">
+	<tr><th>Question Text</th><th>Detailed Link</th></tr>
+	<!-- The varStatus property gives information about the current loop. see http://docs.oracle.com/javaee/6/api/javax/servlet/jsp/jstl/core/LoopTagStatus.html for more info. Remember getIndex translates to variable.index-->
+	<!-- looping through the quizzes ArrayList passed from request.setAttribute, and generate table rows. quiz.qeustionText calls quiz.getQuestionText(), and so on -->
+	<c:forEach items="${quizzes}" var="quiz" varStatus="loop">
+      <tr><td>${quiz.questionText}</td>
+		<td><a href="ViewQuiz?id=${loop.index}"><c:out value="ViewQuiz?id=${loop.index}" /></a></td>      
+      </tr>
+	</c:forEach>
+	</table>
+</body>
+</html>
---
+++ lecture4-backend-j2ee-intro/code/QuizMVC/src/Quizzes.java
@@ -0,0 +1,64 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * loadOneStarup=1, this servlet is the 1st Servlet to be loaded. Normally Servlets are instantiated on the first request, loadOnStartup forces the Servlet to be created on startup
+ * */
+@WebServlet(urlPatterns = "/Quizzes", loadOnStartup = 1)
+public class Quizzes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * Executed exactly one for this servlet
+	 * 
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		ServletContext appContext = getServletContext();
+		Quiz quiz1 = new Quiz("What is 1+1", new String[] { "1", "2", "3", "4" }, 1);
+		Quiz quiz2 = new Quiz("who is the first US president",
+				new String[] { "George Washington", "Michael Hsu", "Kobe Bryant", "Tylor Durden" }, 0);
+		Quiz quiz3 = new Quiz("What is the Answer to the Ultimate Question of Life, the Universe, and Everything", new String[] { "1", "2", "3", "42" }, 3);
+		Quiz quiz4 = new Quiz("What is the average case complexity for merge sort", new String[] { "O(log(n))", "O(n^2)", "O(n log(n))", "O(n)" }, 2);
+		Quiz quiz5 = new Quiz("new quiz", new String[] { "O(log(n))", "O(n^2)", "O(n log(n))", "O(n)" }, 2);
+
+		List<Quiz> quizzes = new ArrayList<Quiz>();
+		quizzes.add(quiz1);
+		quizzes.add(quiz2);
+		quizzes.add(quiz3);
+		quizzes.add(quiz4);
+		quizzes.add(quiz5);
+
+		appContext.setAttribute("quizzes", quizzes);
+	}
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//Getting the list of quizzes
+		List<Quiz> quizzes = (List<Quiz>)getServletContext().getAttribute("quizzes");
+		
+		request.setAttribute("quizzes", quizzes);
+		request.setAttribute("pageHeader", "Quiz List");
+		request.getRequestDispatcher( "/WEB-INF/Quizzes.jsp" )
+				 .forward( request, response );
+	}
+
+}
