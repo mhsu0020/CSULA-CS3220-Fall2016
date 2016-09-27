70ac429eef489a6b5798dc664f8d46e7018e1c5c
Michael Hsu <pig0826usanj@gmail.com>
2016-09-26 17:42:15 -0700

    updated slides for lecture

---
+++ copySlides.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p lecture$1
+find ../../master/CSULA-CS3220-Fall2016/ -name "lecture$1-*" | \
+xargs -i cp -ua {}/slides/. lecture$1
+
+git add .
+git commit -m "updated slides for lecture$1"
+git push -u origin master
