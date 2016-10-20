e7c6f1d60eb2babed4aef11d5985535646896410
Michael Hsu <pig0826usanj@gmail.com>
2016-10-19 17:38:28 -0700

    added DatabaseCofig

--- lecture7-mysql-jdbc/code/EmployeesJDBCExample/src/ListEmployees.java
+++ lecture7-mysql-jdbc/code/EmployeesJDBCExample/src/ListEmployees.java
@@ -22,40 +22,39 @@ public class ListEmployees extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 
-        try{
-            Class.forName( "com.mysql.jdbc.Driver" );
-        }
-        catch( ClassNotFoundException e ){
-            throw new ServletException( e );
-        }
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new ServletException(e);
+		}
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Connection c = null;
 		List<Employee> employees = new ArrayList<>();
-		
+
 		try {
-			
-			c = ConnectionUtils.getMySQLConnection("root", "1234", "localhost", 3306, "test");
+
+			c = ConnectionUtils.getMySQLConnection(DatabaseConfig.MYSQL_USERNAME, DatabaseConfig.MYSQL_PASSWORD,
+					DatabaseConfig.MYSQL_HOST, DatabaseConfig.MYSQL_PORT, DatabaseConfig.MYSQL_DATABASE_TO_USE);
 			Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "select * from employees" );
-            
-            while( rs.next() )
-            {
-            	
-                int id  = rs.getInt("id");
-                String firstName = rs.getString("first_name");
-                String lastName = rs.getString("last_name");
-                String address = rs.getString("address");
-                employees.add(new Employee(id, firstName, lastName, address));
-            }
-			
+			ResultSet rs = stmt.executeQuery("select * from employees");
+
+			while (rs.next()) {
+
+				int id = rs.getInt("id");
+				String firstName = rs.getString("first_name");
+				String lastName = rs.getString("last_name");
+				String address = rs.getString("address");
+				employees.add(new Employee(id, firstName, lastName, address));
+			}
+
 		} catch (SQLException e) {
 			// Escalate to Server error
 			throw new ServletException(e);
-		} 
-		//Always close connections, no matter what happened
+		}
+		// Always close connections, no matter what happened
 		finally {
 			try {
 				if (c != null)
@@ -65,7 +64,6 @@ public class ListEmployees extends HttpServlet {
 			}
 		}
 
-		
 		request.setAttribute("employees", employees);
 		request.getRequestDispatcher("/WEB-INF/ListEmployees.jsp").forward(request, response);
 	}
